f36930c701af46e784830c4ce72014b69ba0996b
EllenBredis <ellen.od.ua@gmail.com>
2022-11-29 17:48:20 +0200

    up

--- src/js/fetch-images.js
+++ src/js/fetch-images.js
@@ -5,7 +5,7 @@ const fetchImages = async (query, page) => {
     const BASE_URL = 'https://pixabay.com/api/'
     const KEY = '31660475-e0e95705a798a5ec8190a2e9f'
     return await fetch(
-      `${BASE_URL}?key=${KEY}&q=${query}&orientation=horizontal&safesearch=true&image_type=photo&per_page=10&page=${page}`
+      `${BASE_URL}?key=${KEY}&q=${query}&orientation=horizontal&safesearch=true&image_type=photo&per_page=40&page=${page}`
     )
       .then(async response => {
         if (!response.ok) {
